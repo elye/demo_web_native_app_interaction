dcef64a32876d3856cbb83ba117cea12b3fb1c80
Elye <elye.project@gmail.com>
2017-07-05 14:04:14 +1000

    Change to Kotlin support.

---
+++ app/src/main/java/com/elyeproj/webappinteraction/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.elyeproj.webappinteraction
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.JavascriptInterface
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        my_web_view.settings.javaScriptEnabled = true
+        my_web_view.addJavascriptInterface(JavaScriptInterface(), JAVASCRIPT_OBJ)
+        my_web_view.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                if (url == BASE_URL) {
+                    injectJavaScriptFunction()
+                }
+            }
+        }
+        my_web_view.loadUrl(BASE_URL)
+
+        btn_send_to_web.setOnClickListener {
+            my_web_view.evaluateJavascript("javascript: " +
+                    "updateFromAndroid(\"" + edit_text_to_web.text + "\")", null)
+        }
+    }
+
+    private fun injectJavaScriptFunction() {
+        my_web_view.loadUrl("javascript: " +
+                "window.androidObj.textToAndroid = function(message) { " +
+                JAVASCRIPT_OBJ + ".textFromWeb(message) }")
+    }
+
+
+    private inner class JavaScriptInterface {
+        @JavascriptInterface
+        fun textFromWeb(fromWeb: String) {
+            txt_from_web.text = fromWeb
+        }
+    }
+
+    companion object {
+        private val JAVASCRIPT_OBJ = "javascript_obj"
+        private val BASE_URL = "file:///android_asset/webview.html"
+    }
+}
